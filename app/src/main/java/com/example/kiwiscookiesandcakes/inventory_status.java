127721ec70addd4cad2ba51a5a1959ca84b86f67
Hayden2147483647 <haydenleojones@gmail.com>
2023-02-09 15:37:32 +1300

    Half of the screens

---
+++ app/src/main/java/com/example/kiwiscookiesandcakes/inventory_status.java
@@ -0,0 +1,14 @@
+package com.example.kiwiscookiesandcakes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inventory_status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_status);
+    }
+}
