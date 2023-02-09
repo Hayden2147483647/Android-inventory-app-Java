66d215d1b31a9fd5cd7822847563172743f5119a
Hayden2147483647 <haydenleojones@gmail.com>
2023-02-09 19:40:41 +1300

    All of the Screens

---
+++ app/src/main/java/com/example/kiwiscookiesandcakes/Admin_Add_Users.java
@@ -0,0 +1,14 @@
+package com.example.kiwiscookiesandcakes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_Add_Users extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_users);
+    }
+}
---
+++ app/src/main/java/com/example/kiwiscookiesandcakes/Admin_Remove_Users.java
@@ -0,0 +1,14 @@
+package com.example.kiwiscookiesandcakes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_Remove_Users extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_remove_users);
+    }
+}
